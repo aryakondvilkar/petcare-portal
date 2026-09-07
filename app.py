29d1837da900ee1a52d351a4698930692c4b2d2a
aryakondvilkar <aryamk2407@gmail.com>
2026-09-07 11:00:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -300,5 +300,9 @@ from reset_and_seed_all import rebuild_database
 if not os.path.exists('petcare.db'):
     rebuild_database()
 
+with app.app_context():
+    import init_db
+    init_db.init_db()
+
 if __name__ == '__main__':
     app.run(debug=True)
